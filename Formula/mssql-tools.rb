4263681428b414d30db5b61acf575dff5b1dc99a
Abhi <abhisheksinha89@users.noreply.github.com>
2017-05-10 12:03:07 -0700

    mssql-tools 14.0.6.0

---
+++ Formula/mssql-tools.rb
@@ -0,0 +1,58 @@
+class MssqlTools < Formula
+  desc "Sqlcmd and Bcp for Microsoft(R) SQL Server(R)"
+  homepage "https://msdn.microsoft.com/en-us/library/ms162773.aspx"
+  url "https://go.microsoft.com/fwlink/?linkid=848963"
+  version "14.0.6.0"
+  sha256 "b31cfe98ff3c8f60a98fd02a1ebbe7cf7a2172320239adccd073ad3870786bf9"
+
+  depends_on "unixodbc"
+  depends_on "openssl"
+  depends_on "msodbcsql"
+
+  def check_eula_acceptance
+    if ENV["ACCEPT_EULA"] != "y" and ENV["ACCEPT_EULA"] != "Y" then
+      puts "The license terms for this product can be downloaded from"
+      puts "http://go.microsoft.com/fwlink/?LinkId=746949 and found in"
+      puts "/usr/local/share/doc/mssql-tools/LICENSE.txt . By entering 'YES',"
+      puts "you indicate that you accept the license terms."
+      puts ""
+      while true do
+        puts "Do you accept the license terms? (Enter YES or NO)"
+        accept_eula = STDIN.gets.chomp
+        if accept_eula then
+          if accept_eula == "YES" then
+            break
+          elsif accept_eula == "NO" then
+            puts "Installation terminated: License terms not accepted."
+            return false
+          else
+            puts "Please enter YES or NO"
+          end
+        else
+          puts "Installation terminated: Could not prompt for license acceptance."
+          puts "If you are performing an unattended installation, you may set"
+          puts "ACCEPT_EULA to Y to indicate your acceptance of the license terms."
+          return false
+        end
+      end
+    end
+    return true
+  end
+
+  def install
+    if !check_eula_acceptance
+      return false
+    end
+
+    chmod 0444, "bin/sqlcmd"
+    chmod 0444, "bin/bcp"
+    chmod 0444, "share/resources/en_US/BatchParserGrammar.dfa"
+    chmod 0444, "share/resources/en_US/BatchParserGrammar.llr"
+    chmod 0444, "share/resources/en_US/bcp.rll"
+    chmod 0444, "share/resources/en_US/SQLCMD.rll"
+    chmod 0644, "usr/share/doc/mssql-tools/LICENSE.txt"
+    chmod 0644, "usr/share/doc/mssql-tools/THIRDPARTYNOTICES.txt"
+
+    cp_r ".", "#{prefix}"
+  end
+end
