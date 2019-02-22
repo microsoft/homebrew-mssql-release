7951ca10d0b24f03bfa1c02d10b13d3cdd2d7a86
David-Engel <dengel1012@gmail.com>
2019-02-21 20:35:19 -0800

    Removing stray spaces

--- Formula/msodbcsql17.rb
+++ Formula/msodbcsql17.rb
@@ -64,7 +64,7 @@ class Msodbcsql17 < Formula
     odbcinst.ini after the formula is uninstalled. This can be done by executing
     the following command:
         odbcinst -u -d -n "ODBC Driver 17 for SQL Server"
-    EOS
+  EOS
   end
 
   test do
