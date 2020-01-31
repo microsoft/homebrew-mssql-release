504ba066cda24be64faa5aba31570e7203992636
Microsoft ODBC Driver for SQL Server <msodbcsql@microsoft.com>
2020-01-31 12:24:25 -0800

    17.5 fixed URLs

--- Formula/msodbcsql17.rb
+++ Formula/msodbcsql17.rb
@@ -1,7 +1,7 @@
 class Msodbcsql17 < Formula
   desc "ODBC Driver for Microsoft(R) SQL Server(R)"
   homepage "https://msdn.microsoft.com/en-us/library/mt654048(v=sql.1).aspx"
-  url "https://download.microsoft.com/download/4/9/5/495639C0-79E4-45A7-B65A-B264071C3D9A/msodbcsql-17.5.1.1.tar.gz"
+  url "https://download.microsoft.com/download/1/9/A/19AF548A-6DD3-4B48-88DC-724E9ABCEB9A/msodbcsql-17.5.1.1.tar.gz"
   version "17.5.1.1"
   sha256 "1e6e884c4a3e93386e018e349f69377700f6eb59e9b0e12c3a440e21da0f22d4"
 
