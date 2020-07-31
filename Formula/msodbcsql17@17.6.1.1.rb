eadd8575adb0d6da8ec1975d504b1be68f9551cf
Microsoft ODBC Driver for SQL Server <msodbcsql@microsoft.com>
2020-07-31 11:20:57 -0700

    17.6.1.1

--- Formula/msodbcsql17@17.6.1.1.rb
+++ Formula/msodbcsql17@17.6.1.1.rb
@@ -1,4 +1,4 @@
-class Msodbcsql17 < Formula
+class Msodbcsql17AT17611 < Formula
   desc "ODBC Driver for Microsoft(R) SQL Server(R)"
   homepage "https://msdn.microsoft.com/en-us/library/mt654048(v=sql.1).aspx"
   url "https://download.microsoft.com/download/1/9/A/19AF548A-6DD3-4B48-88DC-724E9ABCEB9A/msodbcsql-17.6.1.1.tar.gz"
@@ -7,6 +7,8 @@ class Msodbcsql17 < Formula
 
   option "without-registration", "Don't register the driver in odbcinst.ini"
 
+
+  keg_only :versioned_formula
 
   depends_on "unixodbc"
   depends_on "openssl"
@@ -65,7 +67,7 @@ class Msodbcsql17 < Formula
     odbcinst.ini after the formula is uninstalled. This can be done by executing
     the following command:
         odbcinst -u -d -n "ODBC Driver 17 for SQL Server"
-  EOS
+    EOS
   end
 
   test do
